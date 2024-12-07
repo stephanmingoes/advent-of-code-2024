598b037e77f2d868a95c1d3da41a6a3b0f1215ff
Stephan Mingoes <stephanmingoes@gmail.com>
2024-12-07 15:52:08 -0500

    day4:

--- day-4/part-2.ts
+++ day-4/part-2.ts
@@ -1,3 +1,56 @@
+import { puzzleGrid } from "./common";
+
 export default function main() {
-  return 0;
+  const yLength = puzzleGrid.length;
+  const xLength = puzzleGrid[0].length;
+
+  let sum = 0;
+
+  for (let i = 0; i < yLength; i++) {
+    for (let j = 0; j < xLength; j++) {
+      let letter = "A";
+
+      if (puzzleGrid[i][j] !== letter) {
+        continue;
+      }
+
+      // verify that coordinates that make up the x is valid
+
+      let top = puzzleGrid[i - 1];
+      let bottom = puzzleGrid[i + 1];
+
+      if (top !== undefined && bottom !== undefined) {
+        let topRight = top[j + 1];
+        let topLeft = top[j - 1];
+        let bottomRight = bottom[j + 1];
+        let bottomLeft = bottom[j - 1];
+
+        if (
+          (topLeft === "M" &&
+            topRight === "S" &&
+            bottomLeft === "M" &&
+            bottomRight === "S") ||
+          (topLeft === "S" &&
+            topRight === "M" &&
+            bottomLeft === "S" &&
+            bottomRight === "M") ||
+          (topLeft === "S" &&
+            topRight === "S" &&
+            bottomLeft === "M" &&
+            bottomRight === "M") ||
+          (topLeft === "M" &&
+            topRight === "M" &&
+            bottomLeft === "S" &&
+            bottomRight === "S")
+        ) {
+          sum += 1;
+        } else {
+          continue;
+        }
+      } else {
+        continue;
+      }
+    }
+  }
+  return sum;
 }
